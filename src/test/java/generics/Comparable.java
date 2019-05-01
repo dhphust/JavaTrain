38c8dbf2ea43992578942c2648818c901d48760b
dhp <562081472@qq.com>
2019-05-01 11:30:58 +0800

    update project files

--- src/test/java/generics/Comparable.java
+++ src/test/java/generics/Comparable.java
@@ -1,4 +1,40 @@
 package generics;
 
-public interface Comparable {
+import java.util.ArrayList;
+import java.util.List;
+
+public interface Comparable<T> {
+    void test(T t);
+}
+
+/**
+ * 范型擦除
+ */
+class T1 implements Comparable{
+
+    @Override
+    public void test(Object o) {
+
+    }
+}
+
+/**
+ * 父类擦除，子类范型
+ */
+class T3<T> implements Comparable{
+
+    @Override
+    public void test(Object o) {
+
+    }
+}
+
+/**
+ *声明子类，指定具体类型
+ */
+class T2 implements Comparable<String>{
+    @Override
+    public void test(String s) {
+
+    }
 }
--- target/test-classes/ConcurrentPro/README.md
+++
@@ -1,9 +0,0 @@
-## 多线程练习
-
->afasf
-
-* [**this is a test **](README.md)
-
-```
-int count = 0;
-```
