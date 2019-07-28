ceed341f175986f3f8b7eeae0ee66ddf50c25878
dhp <562081472@qq.com>
2019-07-28 08:46:37 +0800

    add leetcode6

---
+++ src/test/java/leetcode/LetterCombinations.java
@@ -0,0 +1,48 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 回溯法实现电话号码字母组合
+ */
+public class LetterCombinations {
+     List<String> result = new ArrayList<>();
+     Map<Character, String> map = new HashMap<Character, String>(){{
+        put('2', "abc");
+        put('3', "def");
+        put('4', "ghi");
+        put('5', "jkl");
+        put('6', "mno");
+        put('7', "pqrs");
+        put('8', "tuv");
+        put('9', "wxyz");
+    }};
+
+    public List<String> letterCombinations(String digits) {
+        if(digits.length() != 0)
+           backtrack(digits.toCharArray(),0,"");
+        return result;
+    }
+
+    private void backtrack(char[] chars, int i, String res) {
+        if(i == chars.length){
+            result.add(res);
+            return;
+        }
+        String letters = map.get(chars[i]);//获取数字对应英文组合
+        for (int j = 0; j < letters.length(); j++) {
+            String letter = letters.substring(j,j+1);
+            backtrack(chars,i+1,res + letter);
+        }
+
+
+    }
+    public static void main(String[] args) {
+        LetterCombinations res = new LetterCombinations();
+        System.out.println(res.letterCombinations("23"));
+
+    }
+}
---
+++ src/test/java/leetcode/PrintAllPermutations.java
@@ -0,0 +1,61 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 打印字符串的所有排序（回溯法）
+ */
+public class PrintAllPermutations {
+    static List<String> list = new ArrayList<>();
+    public static void backtrace(char[] str, int i, String res){
+        if(i == str.length){
+            list.add(res);
+            return;
+        }
+        for (int j = i; j < str.length; j++) {
+            swap(str, i, j);
+            backtrace(str, i + 1,res + String.valueOf(str[i]));//回溯，上次结果res + 本次加入的字符
+            swap(str, i, j);
+        }
+    }
+
+    private static void swap(char[] str, int i, int j) {
+        char tmp = str[i];
+        str[i] = str[j];
+        str[j] = tmp;
+    }
+
+    public static void resolution(String str){
+        backtrace(str.toCharArray(),0,"");
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+        resolution(str);
+        System.out.println(list);
+    }
+
+    /**
+     * 打印字符串的所有子序列（回溯法）
+     */
+//    public class GenerateParenthesis {
+//        static List<String> list = new ArrayList<>();
+//        public static void backtrace(char[] str, int i, String res){
+//            if(i == str.length){
+//                list.add(res);
+//                return;
+//            }
+//            backtrace(str,i+1,res);//回溯，不要本次的字符
+//            backtrace(str,i+1,res + String.valueOf(str[i]));//回溯，上次结果res + 本次加入的字符
+//        }
+//        public static void solution(String str){
+//            backtrace(str.toCharArray(),0,"");
+//        }
+//        public static void main(String[] args) {
+//            String str = "abc";
+//            solution(str);
+//            System.out.println(list);
+//        }
+//    }
+}
