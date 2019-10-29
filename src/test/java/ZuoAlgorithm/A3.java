6c324afbbe4c6ed6b96f96cbd96c872a235c4caf
dhp <562081472@qq.com>
2019-10-29 21:52:25 +0800

    add Zuo A3

---
+++ src/test/java/ZuoAlgorithm/A3.java
@@ -0,0 +1,40 @@
+package ZuoAlgorithm;
+
+import java.util.Stack;
+
+/**
+ * 递归逆序栈
+ */
+public class A3 {
+    //将栈stack的栈底元素返回并移除
+    public static int getAndRemoveLastElement(Stack<Integer> stack){
+        int result = stack.pop();
+        if(stack.isEmpty()){
+            return result;
+        }else{
+            int last = getAndRemoveLastElement(stack);
+            stack.push(result);//除栈底之外的元素压入栈
+            return last;
+        }
+    }
+    //逆序输出栈
+    public static void reverse(Stack<Integer> stack){
+        if(stack.isEmpty())
+            return;
+        //获取栈底元素
+        int i = getAndRemoveLastElement(stack);
+        reverse(stack);
+        stack.push(i);
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(3);
+        stack.push(5);
+        stack.push(4);
+        reverse(stack);
+        for (Integer result : stack) {
+            System.out.print(result + " ");
+        }
+    }
+}
